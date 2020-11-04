122b079c7f2042b0782821bc83771f066509098c
suzuki-gunji <72844589+suzuki-gunji@users.noreply.github.com>
2020-11-04 16:30:14 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,28 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+
+  validates :name,             presence: true
+  validates :explain,          presence: true
+  validates :category_id,      presence: true
+  validates :status_id,        presence: true
+  validates :delivery_fee_id,  presence: true
+  validates :prefecture_id,    presence: true
+  validates :days_id,          presence: true
+  validates :price,            presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :days_id
+
+  validates :category_id,     numericality: { other_than: 1 }
+  validates :status_id,       numericality: { other_than: 1 }
+  validates :delivery_fee_id, numericality: { other_than: 1 }
+  validates :prefecture_id,   numericality: { other_than: 1 }
+  validates :days_id,         numericality: { other_than: 1 }
+
+
 end
