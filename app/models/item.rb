c80619243b5eb2fa0ba9bf05d47ae802d000740d
suzuki-gunji <72844589+suzuki-gunji@users.noreply.github.com>
2020-11-05 14:00:44 +0900

    テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,18 @@
 class Item < ApplicationRecord
-  validates :name,             presence: true
+  has_one_attached :image
+  belongs_to :user
+  
+  validates :name,             presence: true, length: { maximum: 40 } 
   validates :explain,          presence: true
   validates :category_id,      presence: true
   validates :status_id,        presence: true
   validates :delivery_fee_id,  presence: true
   validates :prefecture_id,    presence: true
   validates :days_id,          presence: true
-  validates :price,            presence: true
+
+  validates :price,            presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: { with: /\A[0-9]+\z/ , message: 'には半角の数字を使用してください' }
   validates :image,            presence: true
 
-  has_one_attached :image
-  belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name                   {Faker::Base.regexify("[あ]{2}")}
+    explain                {Faker::Base.regexify("[あ]{2}")}
+    category_id            {Faker::Number.between(from:2, to:11)}
+    status_id              {Faker::Number.between(from:2, to:3)}
+    delivery_fee_id        {Faker::Number.between(from:2, to:3)}
+    prefecture_id          {Faker::Number.between(from:2, to:3)}
+    days_id                {Faker::Number.between(from:2, to:3)}
+    price                  {Faker::Number.between(from:300, to:3000)}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
