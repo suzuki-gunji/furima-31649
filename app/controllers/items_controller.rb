c390a71f2ee4e9bab39f75eaa969e9705611017b
suzuki-gunji <72844589+suzuki-gunji@users.noreply.github.com>
2020-11-06 19:19:01 +0900

    商品詳細表示の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,8 +17,12 @@ class ItemsController < ApplicationController
     end  
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end  
+
   private
   def item_params
-    params.require(:item).permit(:image, :name, :explain, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :days_id, :price ).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :explain, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :day_id, :price ).merge(user_id: current_user.id)
   end  
 end
