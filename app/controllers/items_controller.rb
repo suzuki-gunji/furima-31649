12d1484d442bc4983ff3c818db7f98cad08c7f04
suzuki-gunji <72844589+suzuki-gunji@users.noreply.github.com>
2020-11-09 10:47:15 +0900

    画面上以外からの削除に対応するためのコントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update,]
   before_action :find_params, only: [:show, :edit, :update, :destroy, :move_to_index,]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: [:edit, :destroy,]
   def index
     @items = Item.order("created_at DESC")
   end
