402465dfc327739cd4f1aba7701dd6e210e22776
suzuki-gunji <72844589+suzuki-gunji@users.noreply.github.com>
2020-11-25 12:36:13 +0900

    日本語化したことによるテストコードの修正

--- spec/models/item_purchase_spec.rb
+++ spec/models/item_purchase_spec.rb
@@ -20,47 +20,47 @@ RSpec.describe ItemPurchase, type: :model do
       it 'tokenが空だと登録できない' do
         @item_purchase.token = nil
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Tokenを入力してください")
+        expect(@item_purchase.errors.full_messages).to include("カード情報、有効期限、セキュリティコードを入力してください")
       end
       it 'postal_codeが空だと登録できない' do
         @item_purchase.postal_code = nil
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Postal codeを入力してください")
+        expect(@item_purchase.errors.full_messages).to include("郵便番号を入力してください")
       end
       it 'postal_codeが半角のハイフンを含んだ形式でないと保存できないこと' do
         @item_purchase.postal_code = '1234567'
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Postal codeが間違っています (-)が必要です")
+        expect(@item_purchase.errors.full_messages).to include("郵便番号が間違っています (-)が必要です")
       end
       it 'prefecture_idが空だと登録できない' do
         @item_purchase.prefecture_id = nil
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Prefectureを入力してください")
+        expect(@item_purchase.errors.full_messages).to include("都道府県を入力してください")
       end
       it 'prefecture_idが1だと登録できない' do
         @item_purchase.prefecture_id = 1
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Prefectureは1以外の値にしてください")
+        expect(@item_purchase.errors.full_messages).to include("都道府県は1以外の値にしてください")
       end
       it 'cityが空だと登録できない' do
         @item_purchase.city = nil
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Cityを入力してください")
+        expect(@item_purchase.errors.full_messages).to include("市区町村を入力してください")
       end
       it 'house_numberが空だと登録できない' do
         @item_purchase.house_number = nil
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("House numberを入力してください")
+        expect(@item_purchase.errors.full_messages).to include("番地を入力してください")
       end
       it 'phone_numberが空だと登録できない' do
         @item_purchase.phone_number = nil
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Phone numberを入力してください")
+        expect(@item_purchase.errors.full_messages).to include("電話番号を入力してください")
       end
       it 'phone_numberが12桁だと登録できない' do
         @item_purchase.phone_number = 123456789123
         @item_purchase.valid?
-        expect(@item_purchase.errors.full_messages).to include("Phone numberが間違っています")
+        expect(@item_purchase.errors.full_messages).to include("電話番号が間違っています")
       end
     end
 
